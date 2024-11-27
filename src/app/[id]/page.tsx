896298493a531caf6649de3e9dfd3270a744cb89
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-11-27 14:51:00 +0100

    Feat: implement feature to display a project details after fetchign information from the backend

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -7,16 +7,38 @@ import { Button } from "@nextui-org/react";
 import { RemoveDone, DoneAll } from "@mui/icons-material";
 import AppNavBar from "@/components/ui/navbar";
 
-export default function Page() {
+export default function Page({
+    params,
+  }: {
+    params: Promise<{ id: string }>
+  }) {
+    const [project, setProject] = useState({
+        "title": "",
+        "content": "",
+    });
     const [projectCompleted, setProjectCompleted] = useState(false);
     const [markDownHTML, setMarkDownHTML] = useState("");
     useEffect(() => {
-        async function getHTML() {
-            const processedContent = await remark().use(html).process("#### Resources\n* [Python Basics](https://v1.tailwindcss.com/docs/typography-plugin)\n#### Tasks\n* Set up your development environment and make sure everything is working\n* Install Python on you rlocal machine\n* Install Python on your cloud machine\n* Write a simple \"Hello World!\" program in Python");
-            setMarkDownHTML(processedContent.toString());
-        }
 
-        getHTML();
+        async function getProjectDetails() {
+            const p_id = (await params).id
+
+            try {
+                let res = await fetch(`${process.env.NEXT_PUBLIC_API_URL_V1}/project/fetch/single?q=title,content&id=${p_id}`)
+
+                if (res.ok) {
+                    let project = (await res.json()).data.project;
+                    const processedContent = await remark().use(html).process(project.content);
+                    setMarkDownHTML(processedContent.toString());
+                    setProject(project);
+                } else {
+                    console.log("An error occured!");
+                }
+            } catch (e) {
+                console.log("An error occured!");
+            }
+        }
+        getProjectDetails();
     }, [])
 
     // @ts-ignore
@@ -29,7 +51,7 @@ export default function Page() {
             <div className="mx-6">
                 <p>Welcome Clement,</p>
                 <div className="my-6">
-                    <h3 className="text-lg">Project Title: Python | Setting up {projectCompleted && <DoneAll color="success"/>}</h3>
+                    <h3 className="text-lg">Project Title: {project.title} {projectCompleted && <DoneAll color="success"/>}</h3>
                     <div className="my-4 max-[767px]:prose prose-lg dark:prose-invert" dangerouslySetInnerHTML={{ __html: markDownHTML }}/>
                     {projectCompleted ? <Button onPress={handleProjectStatusUpdate} color="success"> <RemoveDone color="error"/> Mark As Pending</Button>:
                     <Button onPress={handleProjectStatusUpdate} color="success"><DoneAll />Mark As Done</Button>}
