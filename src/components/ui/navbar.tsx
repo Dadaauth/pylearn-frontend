d0ed9eed8280d414fe078da8f59007184ab74d47
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-12-03 17:47:56 +0100

    Feat: update Navbar to display content based on the client Authentication status

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,21 +1,51 @@
+"use client"
+import { checkAuth, fetch_basic_user_details, SignOut } from "@/utils/auth";
 import { Button, Link, Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
+interface User {
+    email?: String,
+    first_name?: String,
+    last_name?: String,
+    role?: String,
+}
 export default function AppNavBar() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [user, setUser] = useState<User>({
+        "email": undefined,
+        "first_name": undefined,
+        "last_name": undefined,
+        "role": undefined,
+    });
+
+    useEffect(() => {
+        (async () => setLoggedIn(await checkAuth()))();
+        (async () => setUser(await fetch_basic_user_details()))();
+    },[])
     return (
         <Navbar>
             <NavbarBrand>
                 <p>A-SWE</p>
             </NavbarBrand>
             <NavbarContent justify="end">
-                <Button
-                    variant="ghost"
-                    as={Link}
-                    href="/auth/login"
-                >Login</Button>
-                <Button
-                    as={Link}
-                    href="/auth/register"
-                >SignUp</Button>
+                {loggedIn?
+                    <>
+                        <p>Welcome {user.first_name}</p>
+                        <Button onPress={() => SignOut()}>Logout</Button>
+                    </>
+                    :
+                    <>
+                        <Button
+                            variant="ghost"
+                            as={Link}
+                            href="/auth/login"
+                        >Login</Button>
+                        <Button
+                            as={Link}
+                            href="/auth/register"
+                        >SignUp</Button>
+                    </>
+                }
             </NavbarContent>
         </Navbar>
     );
