9c3039d8ede1a7b11793580fed0a5d4d0a917fef
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-01-17 14:44:13 +0100

    feat: update the welcome sectio to include the logged in user's first name and the name of the course

--- src/components/ui/welcomeSection.tsx
+++ src/components/ui/welcomeSection.tsx
@@ -1,10 +1,10 @@
-
+import Cookies from "js-cookie";
 
 export default function WelcomeSection() {
     return (
         <div className="mx-6">
-            <h2 className="font-bold text-[#3776AB] text-2xl">Python</h2>
-            <p className="font-medium text-[#2B2D42] mb-6">Welcome back, Clement!</p>
+            <h2 className="font-bold text-[#3776AB] text-2xl">Software Engineering</h2>
+            <p className="font-medium text-[#2B2D42] mb-6">Welcome back, {Cookies.get("first_name")}!</p>
         </div>
     );
 }
